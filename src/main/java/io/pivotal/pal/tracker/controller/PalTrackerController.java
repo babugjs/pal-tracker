910d2499dc32577a48d8d7826dce8411c63896c3
babugjs <babugjs@gmail.com>
2019-10-03 14:22:24 +0530

    Spring Boot application

---
+++ src/main/java/io/pivotal/pal/tracker/controller/PalTrackerController.java
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PalTrackerController {
+
+    @GetMapping ("/")
+        public String sayHello(){
+        return "Hello";
+        }
+}
